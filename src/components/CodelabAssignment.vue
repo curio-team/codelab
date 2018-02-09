79cdaf4c685aa44d34722d26e2f5c3935075739e
Radiuscollege <feddevangils@gmail.com>
2018-02-09 10:09:04 +0100

    fixes #1 added extra assignments;

--- src/components/CodelabAssignment.vue
+++ src/components/CodelabAssignment.vue
@@ -5,7 +5,7 @@
     <div class="assignments">
       <article class="media" v-for="assignment in assignments" >
         <figure class="media-left">
-          <p class="image is-64x64" :style="{background: assignment.level}" style="border-bottom-right-radius: 25px">
+          <p class="image is-64x64 difficulty" :class="assignment.level" style="border-bottom-right-radius: 25px">
             {{assignment.level}}
           </p>
         </figure>
@@ -15,7 +15,7 @@
             <p v-html="assignment.content">
 
             </p>
-            <div class="help">
+            <div class="hulp">
               <h4><b>Hulp</b></h4>
               <p v-html="assignment.hulp"></p>
             </div>
@@ -54,7 +54,53 @@ export default {
             </ul>`
         },
         {
-
+          title: 'Knuffels en kusjes',
+          content: `<p>
+                  Ine ontvangt van haar vriend een aantal berichten met als inhoud iets als: “xoxoxxox”. De  ‘x’ staat voor een kusje en de ‘o’ staat voor een knuffel. Ine wilt graag weten of ze wel evenveel kusjes als knuffels krijgt.  Maak hiervoor een methode (of een functie) die  een boolean returnt en niet hoofdlettergevoelig is.
+                  </p>
+                  `,
+          level: 'bronze',
+          hulp: `
+            De ingevoerde string mag verder allerlei andere woorden en zinnen bevatten. <b> Let op </b>, in de gewone woorden in een berichtje staan uiteraard ook 'x'en en 'o's. Neem die niet mee bij het tellen!
+            `
+        },
+        {
+          title: 'Dubstep DJ part 1',
+          content: `<p>Elton werkt als DJ in de beste nachtclub van Breda. Hij gebruikt vaak Dubstep muziek in zijn performance. Pas geleden heeft hij besloten om wat oude songs te pakken, en er een Dubstep versie van te maken.
+          </p> <p>Laten we aannemen dat een song bestaat uit een bepaalde hoeveelheid woorden. Om een dubstep versie van deze song te maken, voegt Elton een aantal “WUB”’s toe aan een song.</p>
+          <ul>
+          <li>In ieder geval voor het eerste woord van de tekst moet er een random aantal (tussen 0 en 3) keer “WUB” komen te staan. </li>
+          <li>Tussen ieder woord van de tekst moet er (random) minimaal 1 keer “WUB” komen te staan en maximaal 4keer </li>
+          <li>Aan het eind van de song moet er een random aantal (tussen 0 en 3) keer “WUB” komen te staan. </li>
+          </ul>
+          <p>
+          Jij gaat voor hem een programma in de vorm van een methode of functie schrijven die dubstepversies van bestaande songs maakt.
+          </p>
+          `,
+          level: 'silver',
+          hulp: `<p>probeer de opdracht eerst klein te maken met bijvoorbeeld een zin van 5 woorden. Krijg je de goede output, dan lukt het vast ook met een hele lyric van een song</p>
+          <p><b> voorbeeld </b> </p>
+          <p> input: <code> I looked out this morning and the sun was gone </code> <p>
+          </p> outputvoorbeeld: <code> WUBWUB I WUB looked WUBWUBWUB out WUBWUB this WUBWUBWUBWUB morning WUB and WUBWUB the WUB sun WUBWUB was WUBWUBWUB gone WUBWUB </code> </p>
+           `
+        },
+        {
+          title: 'Dubstep DJ part 2',
+          content: `
+          <p> Fer hoort de remixes van Elton. Hij vindt het verschrikkelijk om naar te luisteren. Wel hoort hij vaag de bekende geluiden van songs die hij wel kent. Fer wil graag weten wat die songs zijn. <p></p> Jij gaat helpen om van de Dubstepversies van Elton, de songs terug te brengen naar het origineel, zonder al die irritante “WUB”’s.
+          </p><p>Maak een methode of functie die de remixes van Elton weer terugbrengen in originele staat, inclusief de benodigde spaties, en zonder whitespace voor en achter de song.</p>
+          `,
+          level: 'silver',
+          hulp: `<p>Eigenlijk dus het terugdraaien van wat je in de vorige opdracht hebt gedaan. Let wel op dat er in een lyric eventueel het (deel)woord wub zou kunnen komen te staan.</p><p>Hoe ga jij herkennen dat dit onderdeel is van de originele lyric?</p>`
+        },
+        {
+          title: 'Klinker spel',
+          content:
+          `<p>Mevrouw Rusch geeft in haar vrije tijd extra lessen aan kinderen op het gebied van Taal.
+          Ze is met oefeningen bezig om de kinderen klinkers te leren. Ze heeft als opdracht de kinderen meegegeven om een zin te maken die zoveel mogelijk klinkers bevat.
+          Jij gaat voor haar een programma schrijven die van een gegeven string het <b>aantal</b> klinkers terug geeft.
+          </p>`,
+          level: 'bronze'
         }
       ]
     }
@@ -71,6 +117,29 @@ export default {
 
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css?family=Gloria+Hallelujah');
+
+.difficulty {
+  transform: rotate(-20deg);
+  position: absolute;
+  left: 0px;
+  box-shadow: 3px 3px 3px black;
+  font-family: 'Gloria Hallelujah';
+  color: white;
+  text-shadow: 1px 1px 1px black;
+}
+
+.bronze {
+  background: #CD7F32;
+}
+
+.gold {
+  background : gold;
+}
+
+.silver {
+  background: silver;
+}
 
 article.media {
   background: #dede;
