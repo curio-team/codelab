d620bec6ced55001dacaf4ea1d760bfb1ee0962b
Bart Roos <bart@studiokaa.co>
2018-03-28 09:37:29 +0200

    Clean up left-overs of stage.amo.rocks

--- src/router/index.js
+++ src/router/index.js
@@ -2,9 +2,6 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Skillkaarten from '@/components/Stageopdrachten'
 import Home from '@/components/Home.vue'
-import Planning from '@/components/Planning.vue'
-import Orientatie from '@/components/Orientatie.vue'
-import Checklist from '@/components/Checklist.vue'
 import CodelabAssignment from '@/components/CodelabAssignment.vue'
 
 Vue.use(Router)
@@ -20,21 +17,6 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
-    },
-    {
-      path: '/planning',
-      name: 'Planning',
-      component: Planning
-    },
-    {
-      path: '/orientatieopdracht',
-      name: 'Orientatie',
-      component: Orientatie
-    },
-    {
-      path: '/checklist',
-      name: 'Checklist',
-      component: Checklist
     },
     {
       path: '/codelab-assignments',
